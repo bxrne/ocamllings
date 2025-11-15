8f2a9119bd01faed7b21b4d47ab4d24dd6e9f26c
Adam Byrne <adamrbyrne@gmail.com>
2025-11-15 23:07:20 +0000

    fix(test): added queue tests)

--- test/test_queue.ml
+++ test/test_queue.ml
@@ -1,5 +1,21 @@
 open Alcotest
 
+open Lib
 
 let test_queue () =
-  check bool "test" true true
+  let test_cases = [
+    ([], []);
+    ([1], [1]);
+    ([1; 2; 3; 4; 5], [1; 2; 3; 4; 5]);
+    ([10; 20; 30], [10; 20; 30]);
+  ] in 
+  List.iter (fun (input, expected_output) ->
+    let q = List.fold_left Queue.enqueue (Queue.create ()) input in
+    let rec dequeue_all q acc =
+      match Queue.dequeue q with
+      | None -> List.rev acc
+      | Some (value, q') -> dequeue_all q' (value :: acc)
+    in
+    let output = dequeue_all q [] in
+    check (list int) "Dequeued elements match expected output" expected_output output;
+  ) test_cases
